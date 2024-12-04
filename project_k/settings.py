b19721d0be24760f26eea9fcfdec3b7ba3f344a6
peterstoyanov <peterstoyanov83@gmail.com>
2024-12-04 10:26:39 +0200

    ...

--- project_k/settings.py
+++ project_k/settings.py
@@ -100,7 +100,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / "course_management/static"
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
