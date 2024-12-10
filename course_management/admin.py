d2bed616586e480a57300c74ae6938c419dd79de
peterstoyanov <peterstoyanov83@gmail.com>
2024-12-10 14:21:08 +0200

    Production Ready!!!

--- course_management/admin.py
+++ course_management/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
-from schedule.admin import CalendarAdmin
-from schedule.models import Calendar
 
 from .models import Room, TimeSlot, Course, CourseSchedule, Booking
+
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'capacity')
 
+
 @admin.register(TimeSlot)
 class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ('day', 'start_time')
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'lecturer', 'room')
     search_fields = ('title', 'lecturer__username')
+
 
 @admin.register(CourseSchedule)
 class CourseScheduleAdmin(admin.ModelAdmin):
@@ -23,12 +25,9 @@ class CourseScheduleAdmin(admin.ModelAdmin):
     list_filter = ('status', 'start_date', 'room')
     search_fields = ('course__title', 'room__name')
 
+
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('user', 'course_schedule', 'booking_date')
     list_filter = ('course_schedule__start_date', 'booking_date')
     search_fields = ('user__username', 'course_schedule__course__title')
-
-@admin.register(Calendar)
-class CalendarAdmin(admin.ModelAdmin):
-    pass
