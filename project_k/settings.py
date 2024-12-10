d0274e6801d1b7f85fca7a1828a529020768efe9
peterstoyanov <peterstoyanov83@gmail.com>
2024-12-10 16:19:46 +0200

    Production Ready!!!

--- project_k/settings.py
+++ project_k/settings.py
@@ -125,6 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'client_management.CustomUser'
 
 # Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
