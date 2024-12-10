0e0a55fdc274864c6cd0dd1cd499410dff009d84
peterstoyanov <peterstoyanov83@gmail.com>
2024-12-10 10:23:38 +0200

    Production Ready push

--- project_k/wsgi.py
+++ project_k/wsgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_k.settings')
-
+print("Loading WSGI application...")
+sys.stdout.flush()
 application = get_wsgi_application()
 
+print("WSGI application loaded.")
+sys.stdout.flush()
